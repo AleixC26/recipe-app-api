5aa1a4ca45f4547c1f0fd41a18ee172e063e88b8
Aleix Casals Acien <96aca26@gmail.com>
2025-06-11 09:58:06 +0200

    User model finished

--- app/core/models.py
+++ app/core/models.py
@@ -5,6 +5,7 @@ Database models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django_cryptography.fields import encrypt
 from django_extensions.db.models import TimeStampedModel
+from django.db import models
 
 
 class UserManager(BaseUserManager):
@@ -12,8 +13,19 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user"""
+        if not email:
+            raise ValueError("User must have an email address")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password, username):
+        """Create and return a new superuser"""
+        user = self.create_user(email, password, username=username)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
@@ -24,9 +36,12 @@ class User(AbstractUser, TimeStampedModel):
 
     first_name = encrypt(AbstractUser._meta.get_field("first_name"))
     last_name = encrypt(AbstractUser._meta.get_field("last_name"))
-    email = encrypt(AbstractUser._meta.get_field("email"))
+    email = models.EmailField(blank=True, unique=True)
 
     objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
 
     class Meta(AbstractUser.Meta):
         ordering = ["-date_joined"]
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,6 +4,7 @@ Test for models
 
 from django.core.exceptions import ValidationError
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 from core.factories import UserFactory
 
@@ -34,3 +35,18 @@ class UserTest(TestCase):
         for email, expected in sample_email:
             user = UserFactory.create(email=email)
             self.assertEqual(user.email, expected)
+
+    def test_new_user_withouot_email_raises_error(self):
+        """Test that a user without email raises error"""
+        with self.assertRaises(ValueError):
+            UserFactory.create(email=None)
+
+    def test_create_superuser(self):
+        """Test creating a superuser"""
+        user = get_user_model().objects.create_superuser(
+            "test@example.com",
+            "test123",
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
